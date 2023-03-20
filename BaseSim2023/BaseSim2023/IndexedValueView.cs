9a0b6731beb599a51847658beb98eb40169e1037
qartigala <quentin.artigala@etu.u-bordeaux.fr>
2023-03-20 12:54:46 +0100

    modification des paramètres

--- BaseSim2023/BaseSim2023/IndexedValueView.cs
+++ BaseSim2023/BaseSim2023/IndexedValueView.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BaseSim2023
 {
-    internal class Class1
+    class IndexedValueView
     {
+        public IndexedValue valeur;
+
+        public Point Origine { get; set; } = new Point(0, 0);
+
+        public Size taille { get; set;}
+
+        public Color couleur;
+
+        public int Epaiseur;
+
+
+        public void Dessine(Graphics g)
+        {
+            Point PositionEcran = Origine;
+            Rectangle r = new Rectangle(PositionEcran, taille);
+            Pen p = new Pen(couleur, Epaiseur);
+            g.DrawRectangle(p, r);
+        }
     }
+    
+    
 }
